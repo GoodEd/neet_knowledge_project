6084c4ceef1cf5ddab508e52f61ae0a5974735e2
Jay Prakash <jprakash@goodeducator.com>
2026-03-09 20:24:46 +0530

    feat: add Google Tag Manager (GTM-N5B8N77) from neetprep.com

--- src/utils/ui_helpers.py
+++ src/utils/ui_helpers.py
@@ -24,5 +24,20 @@ def apply_toolbar_style():
     st.markdown(style_block, unsafe_allow_html=True)
 
 
+GTM_HTML = """
+<script>
+window.dataLayer = window.dataLayer || [];
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N5B8N77');
+</script>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5B8N77"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+"""
+
+
 def setup_public_page_chrome():
     apply_toolbar_style()
+    st.markdown(GTM_HTML, unsafe_allow_html=True)
